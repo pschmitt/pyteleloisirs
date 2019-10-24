c8436d6c36705bfbc1d2cf83a7f558a98641bb71
Philipp Schmitt <philipp@schmitt.co>
2019-10-24 14:46:36 +0200

    Blacken

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='pyteleloisirs',
-    version='3.5',
-    license='GPL3',
-    description='Get TV program data from teleloisirs',
-    long_description=open('README.rst').read(),
-    author='Philipp Schmitt',
-    author_email='philipp@schmitt.co',
-    url='https://github.com/pschmitt/pyteleloisirs',
+    name="pyteleloisirs",
+    version="3.5",
+    license="GPL3",
+    description="Get TV program data from teleloisirs",
+    long_description=open("README.rst").read(),
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/pyteleloisirs",
     packages=find_packages(),
-    install_requires=['aiohttp', 'bs4', 'fuzzywuzzy', 'python-Levenshtein'],
-    scripts=['bin/teleloisirs']
+    install_requires=["aiohttp", "bs4", "fuzzywuzzy", "python-Levenshtein"],
+    scripts=["bin/teleloisirs"],
 )
