8dbe822953239decc6f09bf691c7b71f1632e27e
Philipp Schmitt <philipp@schmitt.co>
2019-10-24 14:49:51 +0200

    Fix content type

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     license="GPL3",
     description="Get TV program data from teleloisirs",
     long_description=open("README.rst").read(),
+    long_description_content_type="text/markdown",
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
     url="https://github.com/pschmitt/pyteleloisirs",
